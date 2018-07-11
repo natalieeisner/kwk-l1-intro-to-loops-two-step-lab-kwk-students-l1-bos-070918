8baa29ebb368f87348a091a47948a50f687aeaef
Natalie Eisner <natalieeisner5@gmail.com>
2018-07-11 14:34:06 +0000

    Done.

---
+++ loop_test.rb
@@ -0,0 +1,7 @@
+loop do
+puts "Step to the left"
+puts "Step to the right"
+break
+end
+
+puts "Now jump up and down!"
---
+++ while.rb
@@ -0,0 +1,9 @@
+def hungry_mn
+  kitkat = 100
+  while kitkat > 95
+  puts "Give me another Kitkat!"
+  kitkat -= 1
+end
+end
+
+hungry_mn
